4455ff56b0a0ee650f4db3b733474df579da187f
Saurav Jaiswal <saurav14th@gmail.com>
2023-01-13 19:13:48 +0545

    connecting mongodb with app

---
+++ complete-mongo/db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+
+let dbConnection;
+module.exports = {
+  connectToDb: (cb) => {
+    MongoClient.connect("mongodb://127.0.0.1/bookstore")
+      .then((client) => {
+        dbConnection = client.db();
+        return cb();
+      })
+      .catch((err) => {
+        console.log(err);
+        return cb(err);
+      });
+  },
+  getDb: () => dbConnection,
+};
